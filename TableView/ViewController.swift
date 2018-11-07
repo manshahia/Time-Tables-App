08d725e1c2212eaa2d226334d660a72b8325e318
Ravi Inder Manshahia <ravinder@lab91.co>
2018-11-07 15:13:25 +0530

    TimeTables App complete

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var slider: UISlider!
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        print("slider ", Int(slider.value))
+        table.reloadData()
+    }
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 50
+    }
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String( (Int(slider.value * 20)) * (indexPath.row + 1))
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
